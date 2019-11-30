f1530867f3ab8031c2df872459f2a40853931f9d
IzelKerman <58216235+IzelKerman@users.noreply.github.com>
2019-11-30 14:47:21 +0100

    update 1
    J'ai simplement corrigé des fautes dans le calcul des coordonnées sphériques vers cartésiennes, pris en compte des cas d'erreur pour ces calculs de coordonnées et pour les ajouts de modules sur la fusée. J'ai aussi changé le nom de toutes les fonctions et classes en anglais (bon comme pour l'instant il y en a pas bcp on peut facilement rechanger en anglais mais c'est mieux d'avoir un truc homogène que la moité des fonctions en anglais et l'autre moitié en français)
    
    Ah et j'ai rajouté numpy aussi, il parait que c'est mieux quand on utilise np.array([...]) LoL

--- fusee.py
+++ fusee.py
@@ -1,17 +1,19 @@
 import math
 import scipy
+import numpy as np
 
 #CLASSE DECRIVANT LES DONNEES D UN ETAGE
 class Stage:
-    def __init__(self, M_vide, M_fuel, P, C_A, C, name):
+    def __init__(self, type, M_vide, M_fuel, P, C_A, C):
+        self.type     = type    
         self.M_vide   = M_vide
         self.M_fuel   = M_fuel
         self.P        = P
         self.C_A      = C_A
         self.C        = C
-        self.name     = name
+
 #CLASSE DECRIVANT LA FUSEE UTILISEE
-class Fusee :
+class Rocket:
     def __init__(self):
         self.M        = 0
         self.P        = 0
@@ -20,19 +22,33 @@ class Fusee :
         self.C_boost  = 0
         self.stage    =[]
 
-    def ajouter_module(self, M_vide, M_fuel, P, C_A, C, stage_name):
-        new_stage = Stage( M_vide, M_fuel, P, C_A, C, stage_name)
-        self.stage.append(new_stage)
-        self.M += new_stage.M_vide + new.stage.M_fuel
+    def add_stage(self, stage_type, M_vide, M_fuel, P, C_A, C):
+        #ajoute une étage à la fusée en partant de la charge utile (le haut)
+
+        #test des erreurs possibles
+        if len(self.stage) == 0 and stage_type != 'payload':
+            print("La construction de la fusée doit commencer par de placement de la charge utile.")
+
+        elif self.stage[-1].type != 'payload' and stage_type == 'payload':
+            print("Alors, c'est pas que ça sert à rien de rajouter un payload sous un moteur, mais on va pas se mentir il va cramer ton satellite.")
+
+        elif self.stage[-1].type == 'booster' and stage_type == 'booster':
+            print("Alors Michel va falloir se calmer ça fait vraiment beaucoup de booster la...")
+
+        else:
+            #code de la fonction
+            new_stage = Stage(stage_type, M_vide, M_fuel, P, C_A, C)
+            self.stage.append(new_stage)
+            self.M += new_stage.M_vide + new.stage.M_fuel
 
     def update(self):
-        if self.stage[-1].name == 'booster' :
+        if self.stage[-1].name == 'booster':
             self.P       = self.stage[-1].P + self.stage[-2].P
             self.C_A     = self.stage[-1].C_A + self.stage[-2].C_A
             self.C       = self.stage[-2].C
             self.C_boost = self.stage[-1].C
 
-        elif self.stage[-1].name != 'payload' :
+        elif self.stage[-1].name != 'payload':
             self.P       = self.stage[-1].P
             self.C_A     = self.stage[-1].C_A
             self.C       = self.stage[-1].C
@@ -44,40 +60,55 @@ class Fusee :
         self.update()
 
     def decoupling(self):
+        #détache le dernier étage
         self.M -= self.stage[-1].M_vide
         self.stage.pop()
         self.update()
 
     def stage_time(self):
+        #Calcul la durée dépuisement du fuel de l'étage actuel
         t_stage = self.stage[-1].M_fuel/self.stage[-1].C
         return t_stage
 
     def convert_init(self, position, environnement):
-        #Convertis les coordonnées sphériques en coordonnées cartésiennes à l'initialisation
+        #Converti les coordonnées sphériques en coordonnées cartésiennes à l'initialisation
         x = environnement.r_earth*math.cos(position[0])*math.cos(position[1])
         y = environnement.r_earth*math.cos(position[0])*math.sin(position[1])
         z = environnement.r_earth*math.sin(position[0])
 
         return np.array([x,y,z])
 
-    def convert(self, position):
-        #Convertis les coordonnées sphériques en coordonnées cartésiennes en général pour les calculs
-        r = math.sqrt(sum([x_i**2 for x_i in position]))
-        x = r*math.cos(position[0])*math.cos(position[1])
-        y = r*math.cos(position[0])*math.sin(position[1])
-        z = r*math.sin(position[0])
+    def convert_SC(self, position):
+        #Converti les coordonnées sphériques en coordonnées cartésiennes [r, theta, phi]
+        x = position[0]*math.cos(position[1])*math.cos(position[2])
+        y = position[0]*math.cos(position[1])*math.sin(position[2])
+        z = position[0]*math.sin(position[1])
 
         return np.array([x,y,z])
 
+    def convert_CS(self, position):
+        #Converti les coordonnées cartésiennes en coordonnées sphériques
+        r = math.sqrt(sum([x_i**2 for x_i in position]))
+        if position[0] == 0:
+            phi = math.pi/2 * math.copysign(1, position[1])
+        else:
+            phi = math.atan(position[1]/position[0])
+        if position[0]**2 + position[1]**2 == 0:
+            theta = math.pi/2 * math.copysign(1, position[2])
+        else:
+            theta = math.atan(position[2]/math.sqrt(position[0]**2 + position[1]**2))
+
+        return np.array([r,theta,phi])
+
     def initial_velocity(self, position, environnement):
-        #Convertire la position données en coordonnées sphériques pour le transformer en
-        #vitesse initiale du à la rotation de la Terre
-        r = environnement.r_earth*math.cos(position[0])
-        v = 2*math.pi*environnement.freq_rot*r
+        #Converti la position données en coordonnées sphériques pour le transformer en
+        #vitesse initiale due à la rotation de la Terre
+        r   = environnement.r_earth*math.cos(position[0])
+        v   = 2*math.pi*environnement.freq_rot*r
         v_x = -v*math.sin(position[1])
         v_y =  v*math.cos(position[1])
 
         return np.array([v_x, v_y, 0])
 
-    def affichage(self):
+    def display(self):
         pass
