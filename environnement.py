05719747c8a178988f5242b5c361a2a292dbecf5
Amaury Laridon <amaurylaridon@protonmail.com>
2019-12-06 13:41:34 +0100

    amélioration affichage début

--- environnement.py
+++ environnement.py
@@ -1,8 +1,10 @@
-#CLASSE MODELISANT TOUT L ENVIRONNEMENT DE VOL
-class Environnement :
-    def __init__(self, r_earth, kappa, gk, L_B, T_0, rho_0 = 1.225, T_rot = 86164):
+#--------------------CLASSE MODELISANT TOUT L ENVIRONNEMENT DE VOL----------------#
+class Environment :
+    def __init__(self, G = 6.6743e-11, M_earth = 5.972e24, r_earth = 6371000, gk = 0.03417429, L_B = 0.0065, T_0 = 288.15, rho_0 = 1.225, T_rot = 86164):
+        """Initialise l'environnement dans lequel la fusée va évoluée, par défaut les paramètres sont fixés sur ceux de la Terre"""
+        self.G       = G
+        self.M_earth = M_earth
         self.r_earth = r_earth
-        self.kappa   = kappa
         self.gk      = gk
         self.L_B     = L_B
         self.T_0     = T_0
@@ -11,8 +13,9 @@ class Environnement :
         self.freq_rot  = 1/T_rot
 
     def air_density(self, rayon):
-        h = rayon - r_earth
+        h = rayon - self.r_earth
         exp = 1-self.gk/self.L_B
-        rho = rho_0*(self.T_0/(self.T_0-self.L_B*h))**exp
+        rho = self.rho_0*(self.T_0/abs(self.T_0-self.L_B*h))**exp
+
 
         return rho
