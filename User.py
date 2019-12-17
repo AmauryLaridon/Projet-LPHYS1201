3cc2204c899f0a6fcfda9fda9f54c263b277d75b
IzelKerman <58216235+IzelKerman@users.noreply.github.com>
2019-12-17 12:45:14 +0100

    ajout 1

---
+++ User.py
@@ -0,0 +1,17 @@
+from fusee import *
+from environnement import *
+from graphics import *
+from computer import *
+from parameters import *
+
+# exemple d'utilisation
+rocket = Rocket()
+rocket.create_soyuz_mod()
+
+pc = Computer(rocket)
+pc.launch([45, 0])
+
+GUI = Graphics(pc)
+GUI.display_3D_animation(GUI.animation2)
+GUI.display_2D_animation(GUI.animation_2D)
+GUI.display_multiple_at_once([["plane", "h"], ["g", "m"]], True, False, False)
